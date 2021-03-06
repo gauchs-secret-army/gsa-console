1528c5ba820cebc464de6a6e2bf1e94caf5dfbd4
Reetik Patel <Reetik2000@yahoo.com>
2021-03-06 00:01:38 -0600

    🎉 Implemented init state

--- src/pages/login/component/Login.jsx
+++ src/pages/login/component/Login.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GridLoader from 'react-spinners/GridLoader';
+import FadeLoader from 'react-spinners/FadeLoader';
 
 import styles from "./Login.module.scss";
 import colors from "../../../common/styles/colors.module.scss";
@@ -9,10 +10,29 @@ import classNames from 'classnames';
 export function Login() {
 	const [userID, setUserID] = useState("");
 	const [pass, setPass] = useState("");
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [loginLoading, setLoginLoading] = useState(false);
+	const [isInit, setInit] = useState(false);
+
+	useEffect(() => {
+		fetch("https://gsa-backend-api.herokuapp.com/employees", {
+			method: 'GET',
+			redirect: 'follow'
+		})
+		.then(response => response.json())
+		.then(result => {
+			console.log(result)
+			if(result.length === 0) {
+				setInit(true);
+			}
+		}).catch(error => console.log('error', error))
+		.finally(function() {
+			setLoading(false);
+		});
+	});
 
 	function callLogin() {
-		setLoading(true);
+		setLoginLoading(true);
 		if(validateUser(userID, pass)) {
 			var raw = JSON.stringify({
 				"employeeID": parseInt(userID),
@@ -42,11 +62,11 @@ export function Login() {
 				window.location = "/menu";
 			}).catch(error => console.log('error', error))
 			.finally(function() {
-				setLoading(false);
+				setLoginLoading(false);
 			});
 		} else {
 			alert("User ID or Password is invalid!");
-			setLoading(false);
+			setLoginLoading(false);
 		}
 	}
 
@@ -70,29 +90,53 @@ export function Login() {
 				</div>
 				<div className={classNames(styles.cell, styles.right)}>
 					<div className={styles.loginblock}>
-						<TextField
-							className={styles.loginfield}
-							label="Employee ID"
-							placeholder="Ex. 123456789"
-							pattern="^[0-9]*$"
-							value={userID}
-							onChange={e => setUserID(e.target.value)}
-						/>
-						<TextField
-							className={styles.loginfield}
-							label="Password"
-							type="password"
-							placeholder="********"
-							value={pass}
-							onChange={e => setPass(e.target.value)}
-						/>
+						{ loading ? 
+							<div className={styles.mainLoader}>
+								<FadeLoader 
+									height="25" 
+									width="18"
+									radius="10"
+									margin="15"
+									color={colors.light}
+									css={{ left: 25.5 }}
+								/>
+								<p>Fetching some data...</p>
+							</div> :
 
-						<button className={styles.loginbtn} onClick={() => callLogin()}>
-							{ loading ? 
-								<GridLoader color={colors.light} size={15} />
-								: "Log In"
-							}
-						</button>
+							(isInit ? 
+								<div className={styles.initBlock}>
+									<p>There are no registered users. You can start the registration process below.</p>
+									<button className={styles.loginbtn} onClick={() => window.location = "/employeeDetails"}>
+										Register New User
+									</button>
+								</div> :
+								<div>
+									<TextField
+										className={styles.loginfield}
+										label="Employee ID"
+										placeholder="Ex. 123456789"
+										pattern="^[0-9]*$"
+										value={userID}
+										onChange={e => setUserID(e.target.value)}
+									/>
+									<TextField
+										className={styles.loginfield}
+										label="Password"
+										type="password"
+										placeholder="********"
+										value={pass}
+										onChange={e => setPass(e.target.value)}
+									/>
+
+									<button className={styles.loginbtn} onClick={() => callLogin()}>
+										{ loginLoading ? 
+											<GridLoader color={colors.light} size={15} />
+											: "Log In"
+										}
+									</button>
+								</div>
+							)
+						}
 					</div>
 				</div>
 			</div>
