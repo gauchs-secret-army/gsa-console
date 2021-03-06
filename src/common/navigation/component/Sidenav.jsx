9419f8d683a9c0e78d2ee699f9ce278fa082c564
Reetik Patel <Reetik2000@yahoo.com>
2021-03-05 18:55:55 -0600

    🐛 Fixed sidenav style config

--- src/common/navigation/component/Sidenav.jsx
+++ src/common/navigation/component/Sidenav.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
 import styles from "./Sidenav.module.scss";
 import { LogOut, Truck, UserPlus, BarChart, ShoppingCart, DollarSign } from 'react-feather';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 export function Sidenav() {
     return (
-        <div class = "Sidenav">
-            <div class="Logo" style={{backgroundImage: "url(/img/gsa_logo.png)"}}></div>
-            <div class="Separator"></div>
+        <div className={styles.Sidenav}>
+            <div className={styles.Logo} style={{backgroundImage: "url(/img/gsa_logo.png)"}}></div>
+            <div className={styles.Separator}></div>
             {/*replace the http local host with the link to*/}
-            <a href="http://localhost:3000/transaction" class="SidenavBtn hover">
-                <ShoppingCart class="Icon noHover"/>
-                <div class="SidenavBtnText noHover">Start Transaction</div>
-            </a>
-            <a href="http://localhost:3000/productListing" class="SidenavBtn">
-                <Truck class="Icon noHover"/>
-                <div class="SidenavBtnText noHover">View Products</div>
-            </a>
-            <a href="http://localhost:3000/employeeDetails" class="SidenavBtn">
-                <UserPlus class="Icon noHover"/>
-                <div class="SidenavBtnText noHover">Create Employee</div>
-            </a>
-            <a href="http://localhost:3000/salesReport" class="SidenavBtn">
-                <DollarSign class="Icon noHover"/>
-                <div class="SidenavBtnText noHover">Sales Report</div>
-            </a>
-            <a href="http://localhost:3000/cashierReport" class="SidenavBtn">
-                <BarChart class="Icon noHover"/>
-                <div class="SidenavBtnText noHover">Cashier Report</div>
-            </a>
-            <div class="footer">
-                <div class="Separator"></div>
-                <a href="http://localhost:3000/" class="SignOutBtn">
-                    <div class="SignOutBtnText">
-                        Sign Out<LogOut class="Icon-Exit noHover"/>
+            <Link to="/transaction" className={styles.SidenavBtn}>
+                <ShoppingCart className={classNames(styles.Icon, styles.noHover)}/>
+                <div className={classNames(styles.SidenavBtnText, styles.noHover)}>Start Transaction</div>
+            </Link>
+            <Link to="/productListing" className={styles.SidenavBtn}>
+                <Truck className={classNames(styles.Icon, styles.noHover)}/>
+                <div className={classNames(styles.SidenavBtnText, styles.noHover)}>View Products</div>
+            </Link>
+            <Link to="/employeeDetails" className={styles.SidenavBtn}>
+                <UserPlus className={classNames(styles.Icon, styles.noHover)}/>
+                <div className={classNames(styles.SidenavBtnText, styles.noHover)}>Create Employee</div>
+            </Link>
+            <Link to="/salesReport" className={styles.SidenavBtn}>
+                <DollarSign className={classNames(styles.Icon, styles.noHover)}/>
+                <div className={classNames(styles.SidenavBtnText, styles.noHover)}>Sales Report</div>
+            </Link>
+            <Link to="/cashierReport" className={styles.SidenavBtn}>
+                <BarChart className={classNames(styles.Icon, styles.noHover)}/>
+                <div className={classNames(styles.SidenavBtnText, styles.noHover)}>Cashier Report</div>
+            </Link>
+            <div className={styles.footer}>
+                <div className={styles.Separator}></div>
+                <a onClick={() => window.localStorage.removeItem('user')} className={styles.SignOutBtn}>
+                    <div className={styles.SignOutBtnText}>
+                        Sign Out<LogOut className={classNames(styles.IconExit, styles.noHover)}/>
                     </div>
                 </a>
             </div>
