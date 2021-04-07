5eab500cf47e34b5960ca0e088f8eb4c103a8755
ravenlikescode <77463405+ravenlikescode@users.noreply.github.com>
2021-04-06 20:32:59 -0500

    Introducing errorState

--- src/common/text_field/component/TextField.jsx
+++ src/common/text_field/component/TextField.jsx
@@ -4,10 +4,12 @@ import classNames from 'classnames';
 import styles from "./TextField.module.scss";
 
 export function TextField(props) {
-    var {className, ...props} = props;
+    var {className, errorState, ...props} = props;
     return (
         <div className={classNames(styles.textfield, className)}>
-            <div className={styles.label}> { props.label } </div>
+            <div className={styles.label, {
+                'error': error
+            }}> { props.label } </div>
             <input
                 {...props}
             />
