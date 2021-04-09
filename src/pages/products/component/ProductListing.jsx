9e490699766f5ccafe36f410ff499569d063ffe5
shendrixsee <77458714+shendrixsee@users.noreply.github.com>
2021-04-08 22:07:49 -0500

    Button and item comp

--- src/pages/products/component/ProductListing.jsx
+++ src/pages/products/component/ProductListing.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import styles from "./ProductListing.module.scss";
 import {Sidenav} from "../../../common/navigation/component/Sidenav";
 import { Redirect, useLocation } from 'react-router';
+import classNames from 'classnames';
+import Item from "../../../common/features/component/Item";
+import { Plus } from 'react-feather';
 
+// var Rectangle = document.createElement("Rectangle-13");
+// Rectangle.innerHTML = "Create Item";
+
+
+                
+// var body = document.getElementsByTagName("body")[0];
+// body.appendChild(Rectangle);
+
+                        
+// Rectangle.addEventListener ("click", function() {alert("did something")});
 
 export function ProductListing() {
     const location = useLocation();
@@ -15,10 +28,30 @@ export function ProductListing() {
             <div> 
                 
                 <div className={styles.rootDiv}>
-                    Products: Ibuprofen, Vitamin A tablets, Claritin
+                {
+                    //inline if statement, checking if manager is true or false, if true then button if not then null.
+                    //the {} is a way to get back into javascript mode
+                    user.manager ? 
+                    <div className={classNames(styles.Rectangle, styles.Text)} onClick={()=>alert("Functionality has not yet been implemented")}>
+                    <Plus className={classNames(styles.Icon, styles.noHover)}/>
+                    <div className={classNames(styles.Text)}>Create Item</div>
+                    </div>
+                    : null
+                }
+                {/* <div style="display:block;"> */}
+					<Item
+					className={styles.Rectangle}
+                    //passing to the item component, the only ones ill use are price name and image
+					item={{"productID": 1,
+                    "price": "20.50",
+                    "stock": 1,
+                    "image": "https://images.unsplash.com/photo-1588613254520-9d722c39aad5",
+                    "name": "Benedryll"}}
+					/>
                     {/*This is where everything relating to transaction component in here, 
-                    this is where everything displayed will go, aka like fields*/}
-                
+                    this is where everything displayed will go, aka like fields*/
+                    }
+                {/* </div> */}
                 </div>
                 <Sidenav/>
             </div>
