c96a32cc3f2e3c106d9b88735a31eda821a88f90
Reetik Patel <Reetik2000@yahoo.com>
2021-04-09 20:56:23 -0500

    🎨 Fixed formatting of ProductListing

--- src/pages/products/component/ProductListing.jsx
+++ src/pages/products/component/ProductListing.jsx
@@ -62,28 +62,29 @@ export function ProductListing() {
                     //inline if statement, checking if manager is true or false, if true then button if not then null.
                     //the {} is a way to get back into javascript mode
                     user.manager ? 
-                    <div className={classNames(styles.Rectangle, styles.Text)} onClick={()=>alert("Functionality has not yet been implemented")}>
-                    <Plus className={classNames(styles.Icon, styles.noHover)}/>
-                    <div className={classNames(styles.Text)}>Create Item</div>
-                    </div>
+                        <div className={classNames(styles.Rectangle, styles.Text)} onClick={()=>alert("Functionality has not yet been implemented")}>
+                            <Plus className={classNames(styles.Icon, styles.noHover)}/>
+                            <div className={classNames(styles.Text)}>Create Item</div>
+                        </div>
                     : null
                 }
 					<Item
-					className={styles.Rectangle}
-                    //passing to the item component, the only ones ill use are price name and image
-					item={{"productID": 1,
-                    "price": "20.50",
-                    "stock": 1,
-                    "image": <img className={styles.ItemBubble} src={"https://images.unsplash.com/photo-1588613254520-9d722c39aad5"}/>,
-                    "name": "Benedryll"}}
-					/>
-                    {/*This is where everything relating to transaction component in here, 
-                    this is where everything displayed will go, aka like fields*/
-                    }
-                    Products: { items }
+                        className={styles.Rectangle}
+                        //passing to the item component, the only ones ill use are price name and image
+                        item={{"productID": 1,
+                            "price": "20.50",
+                            "stock": 1,
+                            "image": <img className={styles.ItemBubble} src={"https://images.unsplash.com/photo-1588613254520-9d722c39aad5"}/>,
+                            "name": "Benedryll"
+                        }}
+                    />
+                        {/*This is where everything relating to transaction component in here, 
+                        this is where everything displayed will go, aka like fields*/
+                        }
+                        Products: { items }
 
-                    {/*This is where everything relating to transaction component in here, 
-                    this is where everything displayed will go, aka like fields*/}
+                        {/*This is where everything relating to transaction component in here, 
+                        this is where everything displayed will go, aka like fields*/}
                 </div>
                 <Sidenav/>
             </div>
