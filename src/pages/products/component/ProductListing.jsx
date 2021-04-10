c4a200d841ebf73393da18728c3d57d8989a8fbc
Reetik Patel <Reetik2000@yahoo.com>
2021-04-09 22:22:56 -0500

    💄 Set layout of cards

--- src/pages/products/component/ProductListing.jsx
+++ src/pages/products/component/ProductListing.jsx
@@ -69,16 +69,7 @@ export function ProductListing() {
                         }}
                     />
                 </div>
-                {
-                    //inline if statement, checking if manager is true or false, if true then button if not then null.
-                    //the {} is a way to get back into javascript mode
-                    user.manager ? 
-                        <div className={classNames(styles.Rectangle, styles.Text)} onClick={()=>alert("Functionality has not yet been implemented")}>
-                            <Plus className={classNames(styles.Icon, styles.noHover)}/>
-                            <div className={classNames(styles.Text)}>Create Item</div>
-                        </div>
-                    : null
-                }
+  
                 {/* <Item
                     className={styles.Rectangle}
                     //passing to the item component, the only ones ill use are price name and image
@@ -89,7 +80,19 @@ export function ProductListing() {
                         "name": "Benedryll"
                     }}
                 /> */}
-                { items }
+                <div className={styles.itemList}>
+                    {
+                        //inline if statement, checking if manager is true or false, if true then button if not then null.
+                        //the {} is a way to get back into javascript mode
+                        user.manager ? 
+                            <div className={classNames(styles.Rectangle)} onClick={()=>alert("Functionality has not yet been implemented")}>
+                                <Plus className={classNames(styles.Icon, styles.noHover)}/>
+                                <div className={classNames(styles.Text)}>Create Item</div>
+                            </div>
+                        : null
+                    }
+                    { items }
+                </div>
 
                 <Sidenav/>
             </div>
