056679cfd289a981cfdde044a4a091b9e5cffb24
Joshua <jad041@uark.edu>
2021-04-09 21:31:34 -0500

    Separated the Itemform from the Product Listing Page

--- src/pages/products/component/ProductListing.jsx
+++ src/pages/products/component/ProductListing.jsx
@@ -6,157 +6,16 @@ import { Redirect, useLocation } from 'react-router';
 import Item from "../../../common/features/component/Item";
 import { Plus, Search } from 'react-feather';
 import classNames from 'classnames';
+import {Itemform} from "../../../features/navigation/component/Itemform";
 
 export function ProductListing() {
-    const [loading, setLoading] = useState(false);
-    const [productID, setProductID] = useState("");
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
-    const [img, setImage] = useState(""); //Update
-	const [images, setImages] = useState([]); //for ImgHandler
-    const [stock, setStock] = useState("");
-    const [createdOn, setCreatedOn] = useState("");
-	
-
-	function registerProduct() {
-		setLoading(true);
-		if(validateEntries()) {
-			var rawItem = JSON.stringify({
-				"productID": 2, // Expect this to go away at some point
-				"name": "Pills",
-				"price": "20.50",
-				"image": "1234",   // the stored encoded image string
-				"stock": 1
-			});
-			
-			fetch("https://gsa-backend-api.herokuapp.com/products/new", {
-				method: 'POST',
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: rawItem,
-				redirect: 'follow'
-			}).then(response => response.text())
-			.then(result => {
-				console.log(result);
-				if(result.status === 200) {
-					alert("Item has successfully been registered!");
-					window.localStorage.removeItem('nextRoute');
-					window.location = "/";
-				} 
-				else {
-					alert("Error registering item! Please try again later or contact support at (479) 866-7051.");
-				}
-			})
-			.catch(error => alert("Error registering item! Please try again later or contact sales support at (479) 866-7051."))
-	 		.finally(() => setLoading(false));
-		} 
-		else {
-			setLoading(false);
-		}
-	}
-
-	function clearEntries() {
-		setProductID("");
-		setName("");
-		setPrice("");
-		setImage("");
-		setStock("");
-		setCreatedOn("");
-	}
-
-	function validateEntries() {
-		if(productID === "" || !/^[0-9]{1,5}$/.test(productID)) {
-			alert("Item ID should be a numeric value with 5 or fewer numbers!"); //Update
-			return false;
-		}
-		if(name === "") {
-			alert("Item name should not be empty!");
-			return false;
-		}
-		if(price === "") {
-			alert("Item does not have a price!");
-			return false;
-		}
-		if(img === "") {
-			alert("Item does not have an image!");
-			return false;
-		}
-		return true;
-	}
-
-	function ImgHandler() {
-		
-		const onChange = (imageList, addUpdateIndex) => {
-			setImages(imageList);
-		};
-
-		function hideBtn(hide)
-		{
-			var btn = document.getElementById("UploadButton");
-			if(hide)
-				btn.style.display = "none";
-			else
-				btn.style.display = "inline-block";
-		}
-		
-		return(
-			<div className="ImgHandler">
-				<ImageUploading
-					value = {images}
-					onChange = {onChange}
-					acceptType = {['jpg', 'png', 'jpeg']}
-					dataURLKey = "data_url"
-				>
-					{({imageList, onImageUpload, onImageRemove, isDragging, dragProps}) => (
-						<div className = {styles.image_wrap}>
-							<button 
-								id = {"UploadButton"}
-								className = {styles.button}
-								style = {isDragging ? { color: "#b3cae5" } : null}
-								onClick = {onImageUpload}
-								{...dragProps}
-							>
-								Click or Drop Image Here
-							</button>
-							{imageList.map((image, index) => (
-								<div key = {index} className = {styles.image_item} >
-									<img 
-										className ={styles.image} 
-										src = {image.data_url} 
-										alt = "" width = "300" 
-										onLoad = {() => {
-											setImage(image.data_url);
-											hideBtn(true);
-										}}
-									/>
-									<div className = {styles.button_wrap}>
-										<button 
-											className = {styles.button2} 
-											onClick = {() => {
-												onImageRemove(index); 
-												hideBtn(false);
-											}} 
-										> 
-											X 
-										</button>
-									</div>
-								</div>
-							))}
-						</div>
-					)}
-				</ImageUploading>
-			</div>
-		);
-	}
-
     const location = useLocation();
     window.localStorage.setItem('nextRoute', location.pathname);
     const user = JSON.parse(window.localStorage.getItem('user'));
     const [search, setSearchField] = useState("");
     const [items, setItems] = useState([]);
     var searchTimer = setTimeout(callSearch(), 0);
-
+	
     function callSearch() {
         var raw = JSON.stringify({
             "term": search,
@@ -227,6 +86,7 @@ export function ProductListing() {
                         {/*This is where everything relating to transaction component in here, 
                         this is where everything displayed will go, aka like fields*/}
                 </div>
+				<Itemform/>
                 <Sidenav/>
             </div>
         )
