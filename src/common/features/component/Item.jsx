40d0f86997a2eeb2d8b99772657e9aa2ca75691c
Reetik Patel <Reetik2000@yahoo.com>
2021-04-09 22:06:56 -0500

    🐛🔀 Fixed merge issues in Item

--- src/common/features/component/Item.jsx
+++ src/common/features/component/Item.jsx
@@ -5,12 +5,14 @@ import styles from "./Item.module.scss";
 
 export function Item(props) {
     //can add className to this list, the div item might be div className
-    var {item,className,...props} = props;
+    var {item, className,...props} = props;
     return (
         
-        <div className={classNames(styles.Item, item)}>
+        <div className={classNames(styles.Item, className)}>
             <div className={styles.Rectangle}>
-            <div className={styles.image}> { item.image} </div> 
+            <div className={styles.image}>
+                <img className={styles.ItemBubble} src={item.image}/>
+            </div> 
             <div className={styles.name}> { item.name } </div> 
             <div className={styles.price}> { item.price } </div>
             </div>
